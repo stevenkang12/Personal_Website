f6462854d4f0aeb28dd8cc16e4a34549dd5d297a
Steven <stevenkang12@gmail.com>
2015-12-22 13:43:57 -0500

    Added the resume and projects page

--- models/projects.js
+++ models/projects.js
@@ -2,12 +2,28 @@
 
 var allProjects = [
   {
-    id: 1,
+    id: 3,
     abrev: 'picme',
-    name: 'PicMe (iOS App).',
+    name: 'PicMe (iOS App)',
     time: '2015 - Present',
     description: 'App that focuses on filtered virality of photos and Vine-like videos, allowing users to see content that truly have merit',
-    website: 'picme.io'
+    website: 'http://www.picme.io'
+  },
+  {
+    id: 2,
+    abrev: 'yhack',
+    name: 'YHack (1500 Person National Hackathon).',
+    time: 'Yearly in November. Founded in 2013.',
+    description: 'Co-Founded one of the largest student hackathons in the east coast. In 2014, YHack received 3000 applications and accepted 1500 students from over 5 countries. The motivation is promoting development and meeting new people. If you can create an app in 24 hours, what can\'t you do with a lifetime?',
+    website: 'http://www.yhack.org'
+  },
+  {
+    id: 1,
+    abrev: 'freedomforum',
+    name: 'College Freedom Forum',
+    time: 'Spring 2015',
+    description: 'As president of Yale\'s North Korean human rights group, connected with the Human Rights Foundation to host 4 internationally recognized activist speakers to foster more campus-wide awareness and discussion of human rights',
+    website: 'https://oslofreedomforum.com/events/college-freedom-forum-at-yale-university'
   }
 ];
 
